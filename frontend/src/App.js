fed30494f4bbd6ecd34ee2c4886cb4dea81ce611
Hugo <hugo.carregno@gmail.com>
2019-08-02 03:39:38 -0300

    carpeta frontend con react

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>Agenda</div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//settings
 app.set('port', process.env.PORT || 3000);
 
+//middlewares
+app.use(express.json());
+
+//Starting the server
 app.listen(app.get('port'));
